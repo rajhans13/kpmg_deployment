4e53c7c0863bfc2473a25a0c7753c0ea3543e518
ANURAG RAJHANS <92402899+rajhans13@users.noreply.github.com>
2023-01-10 19:08:21 +0530

    added python project
    added python project

---
+++ test_candidates.py
@@ -0,0 +1,39 @@
+import urllib.request
+
+try:
+    with urllib.request.urlopen('http://localhost:8000/gtg') as gtg:
+        if gtg.code == 200:
+            print(u'\033[90m\N{check mark}\033[0m good to go passed')
+        else:
+            raise Exception
+except Exception as ex:
+    print(u'\033[91m\N{ballot x}\033[0m good to go failed:', ex.reason)
+
+
+try:
+    with urllib.request.urlopen('http://localhost:8000/candidate/John%20Smith', data=b'test') as insert:
+        if insert.code == 200:
+            print(u'\033[90m\N{check mark}\033[0m insert passed')
+        else:
+            raise Exception
+except Exception as ex:
+    print(u'\033[91m\N{ballot x}\033[0m insert failed:', ex.reason)
+
+
+try:
+    with urllib.request.urlopen('http://localhost:8000/candidate/John%20Smith') as verify:
+        if verify.code == 200:
+            print(u'\033[90m\N{check mark}\033[0m verification passed')
+        else:
+            raise Exception(code=verify.code)
+except Exception as ex:
+    print(u'\033[91m\N{ballot x}\033[0m verification failed:', ex.reason)
+
+try:
+    with urllib.request.urlopen('http://localhost:8000/candidates') as list:
+        if list.code == 200:
+            print(u'\033[90m\N{check mark}\033[0m candidate list passed')
+        else:
+            raise Exception
+except Exception as ex:
+    print(u'\033[91m\N{ballot x}\033[0m candidate list failed:', ex.reason)
